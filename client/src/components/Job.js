ac9c388ea6dc4402868b50f38b5c91daa8732540
Campbell Moroney <campbellmoroney@gmail.com>
2022-09-03 21:12:55 +1200

    Created Job.js component

---
+++ client/src/components/Job.js
@@ -0,0 +1,14 @@
+import "./Job.css";
+
+const Job = ({ title, status, date_created, client_name, client_email }) => {
+  return (
+    <li className="job_container">
+      <h3 className="job_title">{title}</h3>
+      <span className="status">{status}</span>
+      <span className="date_created">{date_created}</span>
+      <span className="client_name">{client_name}</span>
+      <span className="client_email">{client_email}</span>
+    </li>
+  );
+};
+export default Job;
